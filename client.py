768574b398408738c5133ef5b1b30868cff105f8
nastiazh <super.nastysha982009@yandex.ua>
2017-02-21 23:19:41 +0200

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from syncercore import File
 from syncercore import Socket
 from syncercore import Syncer
 
-syncer = Syncer('localhost',1234, 'test')
+syncer = Syncer('195.133.145.48',1234, '/usr/bin/python3.5')
 
 #always sync folder every 3 seconds
 while True:
